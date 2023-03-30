da29f4dc43bd00df8a31054a7f16290d9e8df1e5
jrallen724 <jrallen724@gmail.com>
2023-03-29 23:07:19 -0400

    Game gunction is still bugged

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 function getComputerChoice () {
     let num = Math.floor(Math.random() * 3);
-    console.log(num);
     switch(num) {
        case 0:
             return 'rock';
         case 1:
             return 'paper';
         case 2:
-            return 'scissor';
+            return 'scissors';
         default:
             console('There seems to be and error') 
             break;
@@ -15,11 +14,38 @@ function getComputerChoice () {
 }
 
 function getPlayerChoice () {
-    let choice = prompt('Rock paper scissors shoot!').toLowerCase;
-    console.log('player choice: ' + choice);
-    return choice;
+    let choose = prompt('Rock paper scissors shoot!');
+    let playerChoice = choose.toLowerCase();
+    return playerChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
-
+    console.log('Player chooses ' + playerSelection + ' and computer chooses ' + computerSelection);
+    if (playerSelection === computerSelection) {
+        return 'Tie game. Try again.';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win!';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win!';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You win!';
+    } else {
+        return 'You loose!'
+    }
 }
+
+function game (num) {
+    let userScore = 0;
+    let computerScore = 0;
+    while (num > 0) {
+        if (playRound(getPlayerChoice(), getComputerChoice()) === 'You win!') {
+            userScore += 1;
+            num --;
+        } else if(playRound(getPlayerChoice(), getComputerChoice()) === 'You loose!') {
+            computerScore += 1;
+            num --;
+        }
+    }
+}
+
+game (5);
