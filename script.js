fe01a8d4ed35964bf8bd6226cb1a71e96e4145e0
Jeff_Allen <jrallen724@gmail.com>
2023-03-30 18:16:38 -0400

    need to finish game function

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getComputerChoice () {
         case 1:
             return 'paper';
         case 2:
-            return 'scissor';
+            return 'scissors';
         default:
             console('There seems to be and error') 
             break;
@@ -15,11 +15,31 @@ function getComputerChoice () {
 }
 
 function getPlayerChoice () {
-    let choice = prompt('Rock paper scissors shoot!').toLowerCase;
+    let choice = prompt('Rock paper scissors shoot!').toLowerCase();
     console.log('player choice: ' + choice);
     return choice;
 }
 
 function playRound(playerSelection, computerSelection) {
-
+    console.log('You chose ' + playerSelection + ' and the computer chose ' + computerSelection + '.')
+    if (playerSelection === computerSelection) {
+        return 'Tie game.Try again';
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win!';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win!';
+    } else if (playerSelection === 'scissors' && computerSelection === 'pap er') {
+        return 'You win!';
+    } else {
+        return 'You loose!'
+    }
 }
+
+/* game (num) {
+    let playerCount = 0;
+    let computerCount = 0;
+    
+    while (playerCount < num || computerCount < num) {}
+} */
+
+playRound(getPlayerChoice(), getComputerChoice());
